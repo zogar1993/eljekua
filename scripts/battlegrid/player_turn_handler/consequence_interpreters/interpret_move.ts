62a219da984246c6c65c4a23f9efbf1a48ed883a
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-07-24 13:28:19 -0300

    :recycle: make opportunity attack creation a little tidier

--- scripts/battlegrid/player_turn_handler/consequence_interpreters/interpret_move.ts
+++ scripts/battlegrid/player_turn_handler/consequence_interpreters/interpret_move.ts
@@ -29,7 +29,7 @@ export const interpret_move = ({
             battle_grid.move_creature_one_square({creature, position: new_position})
         } else {
             for (const attacker of potential_attackers) {
-                const consequences = add_option_for_opportunity_attack(remove_first_targeting(BASIC_ATTACK_ACTIONS[0].consequences))
+                const consequences = turn_power_into_opportunity_attack(BASIC_ATTACK_ACTIONS[0].consequences)
                 const name = BASIC_ATTACK_ACTIONS[0].name
                 turn_context.add_power_context({name, consequences, owner: attacker})
                 turn_context.get_current_context().set_creature({name: "primary_target", value: creature})
@@ -49,22 +49,21 @@ export const interpret_move = ({
     }
 }
 
-//TODO make it tidier
+const turn_power_into_opportunity_attack = (consequences: Array<Consequence>) =>
+    add_option_for_opportunity_attack(remove_first_targeting(consequences))
+
 const remove_first_targeting = (consequences: Array<Consequence>) => {
     if (consequences[0].type === "select_target")
         return consequences.slice(1)
     throw Error("targeting needed for removing it")
 }
 
-//TODO make it tidier
-const add_option_for_opportunity_attack = (consequences: Array<Consequence>): Array<Consequence> => {
-    return [
-        {
-            type: "options",
-            options: [
-                {text: "Opportunity Attack", consequences},
-                {text: "Ignore", consequences: []},
-            ],
-        }
-    ]
-}
+const add_option_for_opportunity_attack = (consequences: Array<Consequence>): Array<Consequence> => [
+    {
+        type: "options",
+        options: [
+            {text: "Opportunity Attack", consequences},
+            {text: "Ignore", consequences: []},
+        ],
+    }
+]
