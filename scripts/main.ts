4c7460d31766e9a019e1d3080234310f0296e6ce
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-08-06 15:14:36 -0300

    :sparkles: add demo initialization

--- scripts/main.ts
+++ scripts/main.ts
@@ -22,64 +22,66 @@ visual_creature_creator.addOnCreatureHoverEvent(player_turn_handler.on_hover)
 visual_square_creator.addOnSquareHoverEvent(player_turn_handler.on_hover)
 
 const ATTRIBUTES = {
-    STRENGTH: "str",
-    CONSTITUTION: "con",
-    DEXTERITY: "dex",
-    INTELLIGENCE: "int",
-    WISDOM: "wis",
-    CHARISMA: "cha",
-} as const
+        STRENGTH: "str",
+        CONSTITUTION: "con",
+        DEXTERITY: "dex",
+        INTELLIGENCE: "int",
+        WISDOM: "wis",
+        CHARISMA: "cha",
+    } as const
 
-const bob = {
-    name: "Bob",
-    position: {x: 1, y: 2},
-    image: `url("/public/war-axe.svg")`,
-    movement: 5,
-    hp_current: 7,
-    hp_max: 10,
-    level: 2,
-    attributes: Object.fromEntries(Object.values(ATTRIBUTES).map(attr => [attr, 14])) as Creature["data"]["attributes"],
-    powers: FIGHTER_POWERS
-}
-const maik = {
-    name: "Maik",
-    position: {x: 2, y: 5},
-    image: `url("/public/wizard-staff.svg")`,
-    movement: 2,
-    hp_current: 10,
-    hp_max: 10,
-    level: 1,
-    attributes: Object.fromEntries(Object.values(ATTRIBUTES).map(attr => [attr, 14])) as Creature["data"]["attributes"],
-    powers: WIZARD_POWERS
-}
-const yeims = {
-    name: "Yeims",
-    position: {x: 2, y: 6},
-    image: `url("/public/crossbow.svg")`,
-    movement: 10,
-    hp_current: 10,
-    hp_max: 10,
-    level: 1,
-    attributes: Object.fromEntries(Object.values(ATTRIBUTES).map(attr => [attr, 14])) as Creature["data"]["attributes"],
-    powers: []
-}
+;(window as any).init_demo = () => {
+    const bob = {
+        name: "Bob",
+        position: {x: 1, y: 2},
+        image: `url("/public/war-axe.svg")`,
+        movement: 5,
+        hp_current: 7,
+        hp_max: 10,
+        level: 2,
+        attributes: Object.fromEntries(Object.values(ATTRIBUTES).map(attr => [attr, 14])) as Creature["data"]["attributes"],
+        powers: FIGHTER_POWERS
+    }
+    const maik = {
+        name: "Maik",
+        position: {x: 2, y: 5},
+        image: `url("/public/wizard-staff.svg")`,
+        movement: 2,
+        hp_current: 10,
+        hp_max: 10,
+        level: 1,
+        attributes: Object.fromEntries(Object.values(ATTRIBUTES).map(attr => [attr, 14])) as Creature["data"]["attributes"],
+        powers: WIZARD_POWERS
+    }
+    const yeims = {
+        name: "Yeims",
+        position: {x: 2, y: 6},
+        image: `url("/public/crossbow.svg")`,
+        movement: 10,
+        hp_current: 10,
+        hp_max: 10,
+        level: 1,
+        attributes: Object.fromEntries(Object.values(ATTRIBUTES).map(attr => [attr, 14])) as Creature["data"]["attributes"],
+        powers: []
+    }
 
-const jenri = {
-    name: "Jenri",
-    position: {x: 9, y: 2},
-    image: `url("/public/saber-and-pistol.svg")`,
-    movement: 10,
-    hp_current: 10,
-    hp_max: 10,
-    level: 1,
-    attributes: Object.fromEntries(Object.values(ATTRIBUTES).map(attr => [attr, 14])) as Creature["data"]["attributes"],
-    powers: ROGUE_POWERS
-}
+    const jenri = {
+        name: "Jenri",
+        position: {x: 9, y: 2},
+        image: `url("/public/saber-and-pistol.svg")`,
+        movement: 10,
+        hp_current: 10,
+        hp_max: 10,
+        level: 1,
+        attributes: Object.fromEntries(Object.values(ATTRIBUTES).map(attr => [attr, 14])) as Creature["data"]["attributes"],
+        powers: ROGUE_POWERS
+    }
 
-battle_grid.create_creature(bob)
-battle_grid.create_creature(maik)
-battle_grid.create_creature(yeims)
-battle_grid.create_creature(jenri)
+    battle_grid.create_creature(bob)
+    battle_grid.create_creature(maik)
+    battle_grid.create_creature(yeims)
+    battle_grid.create_creature(jenri)
+}
 
 ;(window as any).add_character = (data: CreatureData) => {
     battle_grid.create_creature(data)
