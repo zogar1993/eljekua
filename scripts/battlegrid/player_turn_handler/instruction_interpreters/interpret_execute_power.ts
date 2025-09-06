66ae6a0308e1bea88895dd3ddc548a7d0dd3380d
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-09-05 21:59:37 -0300

    :fire: remove unused imports

--- scripts/battlegrid/player_turn_handler/instruction_interpreters/interpret_execute_power.ts
+++ scripts/battlegrid/player_turn_handler/instruction_interpreters/interpret_execute_power.ts
@@ -1,8 +1,7 @@
-import {InstructionExecutePower, PowerVM} from "tokenizer/transform_power_ir_into_vm_representation";
+import {InstructionExecutePower} from "tokenizer/transform_power_ir_into_vm_representation";
 import {
     InterpretInstructionProps
 } from "battlegrid/player_turn_handler/instruction_interpreters/InterpretInstructionProps";
-import {PowerContext} from "battlegrid/player_turn_handler/PowerContext";
 
 export const interpret_execute_power = ({
                                             instruction,
