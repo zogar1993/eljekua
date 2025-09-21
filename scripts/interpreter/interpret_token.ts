2c36e4c91b7858d4d05ad262bb5eada981fb0518
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-09-21 17:35:22 -0300

    :truck: move interpret_function into specific interpreters

--- scripts/interpreter/interpret_token.ts
+++ scripts/interpreter/interpret_token.ts
@@ -1,22 +1,13 @@
 import {roll_d} from "randomness/dice";
 import {Token} from "tokenizer/tokens/AnyToken";
-import {TokenFunction} from "tokenizer/tokens/TokenFunction";
 import {is_number, is_number_resolved} from "interpreter/add_numbers";
-import {
-    interpret_token_function_not_equals
-} from "interpreter/specific_interpreters/interpret_token_function_not_equals";
 import {AstNode, AstNodeNumber, AstNodeNumberResolved, InterpretProps} from "interpreter/types";
-import {
-    token_to_has_valid_targets_function_node
-} from "interpreter/specific_interpreters/interpret_token_function_has_valid_targets";
 import {interpret_token_keyword} from "interpreter/specific_interpreters/interpret_token_keyword";
-import {interpret_token_function_equipped} from "interpreter/specific_interpreters/interpret_token_function_equipped";
 import {interpret_token_string} from "interpreter/specific_interpreters/interpret_token_string";
-import {interpret_token_function_exists} from "interpreter/specific_interpreters/interpret_token_function_exists";
-import {interpret_token_function_add} from "interpreter/specific_interpreters/interpret_token_function_add";
 import {interpret_number} from "interpreter/specific_interpreters/interpret_number";
 import {interpret_weapon} from "interpreter/specific_interpreters/interpret_weapon";
 import {interpret_dice} from "interpreter/specific_interpreters/interpret_dice";
+import {interpret_function} from "interpreter/specific_interpreters/interpret_function";
 
 export const resolve_number = (number: AstNodeNumber): AstNodeNumberResolved => {
     if (is_number_resolved(number)) return number
@@ -34,7 +25,7 @@ export const resolve_number = (number: AstNodeNumber): AstNodeNumberResolved => 
 export const interpret_token = ({token, ...props}: InterpretProps<Token>): AstNode => {
     switch (token.type) {
         case "function":
-            return token_to_function_node({token, ...props})
+            return interpret_function({token, ...props})
         case "number":
             return interpret_number({token, ...props})
         case "string":
@@ -45,23 +36,6 @@ export const interpret_token = ({token, ...props}: InterpretProps<Token>): AstNo
             return interpret_dice({token, ...props})
         case "keyword":
             return interpret_token_keyword({token, ...props})
-    }
-}
-
-const token_to_function_node = ({token, ...props}: InterpretProps<TokenFunction>): AstNode => {
-    switch (token.name) {
-        case "add":
-            return interpret_token_function_add({token, ...props})
-        case "exists":
-            return interpret_token_function_exists({token, ...props})
-        case "equipped":
-            return interpret_token_function_equipped({token, ...props})
-        case "not_equals":
-            return interpret_token_function_not_equals({token, ...props})
-        case "has_valid_targets":
-            return token_to_has_valid_targets_function_node({token, ...props})
-        default:
-            throw Error(`function name ${token.name} not supported`)
     }
 }
 
