099f49171865d325dbc9734ca51264479df4658c
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-09-27 23:51:30 -0300

    :sparkles: allow for weapons and dice to have more than one on a single roll

--- scripts/expressions/token_evaluator/internals/evaluate_dice.ts
+++ scripts/expressions/token_evaluator/internals/evaluate_dice.ts
@@ -1,5 +1,11 @@
 import type {AstNodeNumberUnresolved} from "expressions/token_evaluator/types";
 import type {DiceToken} from "expressions/tokenizer/tokens/RollToken";
+import {add_numbers} from "expressions/token_evaluator/add_numbers";
 
-export const evaluate_dice = (token: DiceToken): AstNodeNumberUnresolved =>
-    ({type: "number_unresolved", min: 1, max: token.faces, description: `${token.faces}d${token.faces}`})
+export const evaluate_dice = (token: DiceToken): AstNodeNumberUnresolved => {
+    const description = `d${token.faces}`
+    const node: AstNodeNumberUnresolved = {type: "number_unresolved", min: 1, max: token.faces, description}
+    return token.amount === 1 ?
+        node :
+        {...add_numbers(Array.from({length: token.amount}, () => node)), description: `${token.faces}${description}`}
+}
--- scripts/expressions/token_evaluator/internals/evaluate_weapon.ts
+++ scripts/expressions/token_evaluator/internals/evaluate_weapon.ts
@@ -1,5 +1,9 @@
 import type {AstNodeNumberUnresolved} from "expressions/token_evaluator/types";
 import type {WeaponToken} from "expressions/tokenizer/tokens/RollToken";
+import {add_numbers} from "expressions/token_evaluator/add_numbers";
 
-export const evaluate_weapon = (token: WeaponToken): AstNodeNumberUnresolved =>
-    ({type: "number_unresolved", min: 1, max: 4, description: `${token.amount}W`})
+export const evaluate_weapon = (token: WeaponToken): AstNodeNumberUnresolved => {
+    const node: AstNodeNumberUnresolved = {type: "number_unresolved", min: 1, max: 4, description: `W`}
+    return {...add_numbers(Array.from({length: token.amount}, () => node)), description: `${token.amount}W`}
+}
+
