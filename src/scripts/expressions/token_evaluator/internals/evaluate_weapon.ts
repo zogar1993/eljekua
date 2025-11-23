ef5eb008202fecfef9faf462aff039751bd703ee
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-11-23 16:47:17 -0300

    :loud_sound: action logs for one weapon die now are unwrapped

--- src/scripts/expressions/token_evaluator/internals/evaluate_weapon.ts
+++ src/scripts/expressions/token_evaluator/internals/evaluate_weapon.ts
@@ -4,6 +4,7 @@ import {number_utils} from "scripts/expressions/token_evaluator/number_utils";
 
 export const evaluate_weapon = (token: WeaponToken): ExprNumberUnresolved => {
     const die: ExprNumberUnresolved = {type: "number_unresolved", min: 1, max: 4, description: `W`}
+    if (token.amount === 1) return die
     return {...number_utils(Array.from({length: token.amount}, () => die)), description: `${token.amount}W`}
 }
 
