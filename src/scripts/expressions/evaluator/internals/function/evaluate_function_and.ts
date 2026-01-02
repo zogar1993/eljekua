e9a2c7758a82e5443d8cec48e64fcd5da1468848
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-01-01 21:34:39 -0300

    :construction: add "and" function

---
+++ src/scripts/expressions/evaluator/internals/function/evaluate_function_and.ts
@@ -0,0 +1,27 @@
+import type {AstNodeFunction} from "scripts/expressions/parser/nodes/AstNodeFunction";
+import type {ExprBoolean} from "scripts/expressions/evaluator/types";
+import {assert_parameters_amount_is_at_least} from "scripts/expressions/evaluator/asserts";
+import type {AstNode} from "scripts/expressions/parser/nodes/AstNode";
+import type {Expr} from "scripts/expressions/evaluator/types";
+
+export const evaluate_function_and = ({node, evaluate_ast}:
+                                         {
+                                             node: AstNodeFunction
+                                             evaluate_ast: (node: AstNode) => Expr
+                                         }): ExprBoolean => {
+    assert_parameters_amount_is_at_least(node, 2)
+
+    const parameters = node.parameters.map(evaluate_ast)
+
+    if (!parameters.every(parameter => parameter.type === "boolean"))
+        throw Error(`Expected all '$and()' parameters to evaluate to booleans, but found '${JSON.stringify(parameters)}'`)
+
+    const result = parameters.every(parameter => parameter.value)
+
+    return {
+        type: "boolean",
+        value: result,
+        description: "not_equals",
+        params: parameters
+    }
+}
