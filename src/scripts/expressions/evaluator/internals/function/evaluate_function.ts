54ef31036fa766380da1e6804cf81ac349b5ba2b
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-01-01 21:51:20 -0300

    :sparkles: add interpretation for "can_expend_action_type" and "and" functions

--- src/scripts/expressions/evaluator/internals/function/evaluate_function.ts
+++ src/scripts/expressions/evaluator/internals/function/evaluate_function.ts
@@ -12,11 +12,14 @@ import {evaluate_function_or} from "scripts/expressions/evaluator/internals/func
 import type {AstNode} from "scripts/expressions/parser/nodes/AstNode";
 import type {TurnState} from "scripts/battlegrid/player_turn_handler/TurnState";
 import {evaluate_function_exists} from "scripts/expressions/evaluator/internals/function/evaluate_function_exists";
-import type {PlayerTurnHandler} from "scripts/battlegrid/player_turn_handler/PlayerTurnHandler";
 import {
     evaluate_function_is_greater_or_equal
 } from "scripts/expressions/evaluator/internals/function/evaluate_function_is_greater_or_equal";
 import {BattleGrid} from "scripts/battlegrid/BattleGrid";
+import {
+    evaluate_function_can_expend_action_type
+} from "scripts/expressions/evaluator/internals/function/evaluate_function_can_expend_action_type";
+import {evaluate_function_and} from "scripts/expressions/evaluator/internals/function/evaluate_function_and";
 
 export const build_evaluate_function = ({evaluate_ast, turn_state, battle_grid}:
                                             {
@@ -28,19 +31,23 @@ export const build_evaluate_function = ({evaluate_ast, turn_state, battle_grid}:
     return (node: AstNodeFunction): Expr => {
         switch (node.name) {
             case "add":
-                return evaluate_function_add({node: node, evaluate_ast: evaluate_ast})
+                return evaluate_function_add({node, evaluate_ast})
             case "exists":
-                return evaluate_function_exists({node: node, turn_state})
+                return evaluate_function_exists({node, turn_state})
             case "equipped":
-                return evaluate_function_equipped({node: node, evaluate_ast: evaluate_ast})
+                return evaluate_function_equipped({node, evaluate_ast})
             case "not_equals":
-                return evaluate_function_not_equals({node: node, evaluate_ast: evaluate_ast})
+                return evaluate_function_not_equals({node, evaluate_ast})
             case "has_valid_targets":
-                return evaluate_function_has_valid_targets({node: node, turn_state, evaluate_ast, battle_grid})
+                return evaluate_function_has_valid_targets({node, turn_state, evaluate_ast, battle_grid})
+            case "can_expend_action_type":
+                return evaluate_function_can_expend_action_type({node, evaluate_ast})
             case "or":
-                return evaluate_function_or({node: node, evaluate_ast: evaluate_ast})
+                return evaluate_function_or({node, evaluate_ast})
+            case "and":
+                return evaluate_function_and({node, evaluate_ast})
             case "is_greater_or_equal":
-                return evaluate_function_is_greater_or_equal({node: node, evaluate_ast: evaluate_ast})
+                return evaluate_function_is_greater_or_equal({node, evaluate_ast})
             default:
                 throw Error(`function name '${node.name}' not supported when evaluating node`)
         }
