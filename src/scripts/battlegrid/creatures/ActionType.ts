fb993209c542bf6659d2d4402cb754b75a1f37e7
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-01-01 21:32:40 -0300

    :technologist: add helpers for turn actions

--- src/scripts/battlegrid/creatures/ActionType.ts
+++ src/scripts/battlegrid/creatures/ActionType.ts
@@ -7,3 +7,11 @@ export const ACTION_TYPE = {
 } as const
 
 export type ActionType = typeof ACTION_TYPE[keyof typeof ACTION_TYPE]
+
+export const TURN_ACTION_TYPES = [ACTION_TYPE.MINOR, ACTION_TYPE.MOVEMENT, ACTION_TYPE.STANDARD] as const
+
+export function assert_is_action_type(value: string): asserts value is ActionType {
+    for (const action_type of Object.values(ACTION_TYPE))
+        if (action_type === value) return
+    throw Error(`"${value}" is not an action type`)
+}
