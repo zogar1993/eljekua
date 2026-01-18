c54eab918d46eeacb746b192d2dc82c3d56da13c
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-01-18 12:37:14 -0300

    :coffin: delete turn action types array

--- src/scripts/battlegrid/creatures/ActionType.ts
+++ src/scripts/battlegrid/creatures/ActionType.ts
@@ -16,8 +16,6 @@ export const ACTION_TYPE_EXPENDITURE_ORDER = {
 
 export type ActionType = typeof ACTION_TYPE[keyof typeof ACTION_TYPE]
 
-export const TURN_ACTION_TYPES = [ACTION_TYPE.MINOR, ACTION_TYPE.MOVEMENT, ACTION_TYPE.STANDARD] as const
-
 export function assert_is_action_type(value: string): asserts value is ActionType {
     for (const action_type of Object.values(ACTION_TYPE))
         if (action_type === value) return
