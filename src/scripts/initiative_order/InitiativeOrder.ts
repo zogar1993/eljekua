96332d8688853e574793f094c05f0592c4e873f4
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-03-01 19:48:29 -0300

    :recycle: rework how initiative order works to make it more straightforward

--- src/scripts/initiative_order/InitiativeOrder.ts
+++ src/scripts/initiative_order/InitiativeOrder.ts
@@ -1,44 +1,53 @@
 import {Creature} from "scripts/battlegrid/creatures/Creature";
 import {roll_d} from "scripts/randomness/dice";
-import {InitiativeOrderVisual} from "scripts/initiative_order/InitiativeOrderVisual";
+import {CreatureInitiativeVisual, InitiativeOrderVisual} from "scripts/initiative_order/InitiativeOrderVisual";
 import {ExprNumberResolved} from "scripts/expressions/evaluator/types";
+import {insert_to_array} from "scripts/ts_utils/insert_to_array";
 
 export const create_initiative_order = ({visual_initiative_order}: {
     visual_initiative_order: InitiativeOrderVisual
 }) => {
-    let initiative_order: Array<{ creature: Creature, initiative: ExprNumberResolved, visual: HTMLDivElement }> = []
-
+    let initiatives: Array<{
+        creature: Creature,
+        initiative: ExprNumberResolved,
+        visual: CreatureInitiativeVisual
+    }> = []
     let current_index = 0
 
-
     const add_creature = (creature: Creature) => {
+        //TODO contemplate current creature changing mid turn?
+        //TODO contemplate same initiative
         const initiative = roll_d(20)
-        const visual_creature = visual_initiative_order.create_creature({creature, initiative})
-        initiative_order.push({creature, initiative, visual: visual_creature})
+
+        let index = 0
+        while (index < initiatives.length) {
+            const entry = initiatives[index]
+            if (initiative.value > entry.initiative.value)
+                break
+            index++
+        }
+        const visual_creature = visual_initiative_order.create_creature({creature, initiative, index})
+        const new_entry = {creature, initiative, visual: visual_creature}
+        insert_to_array(initiatives, new_entry, index)
     }
 
     const get_current_creature = (): Creature => {
-        if (current_index >= initiative_order.length)
+        if (current_index >= initiatives.length)
             throw Error(`Initiative index ${current_index} out of bounds`)
-        return initiative_order[current_index].creature
+        return initiatives[current_index].creature
     }
 
     const next_turn = () => {
-        initiative_order[current_index].visual.removeAttribute("current-turn")
-        if (current_index + 1 === initiative_order.length)
+        initiatives[current_index].visual.set_current_turn(false)
+        if (current_index + 1 === initiatives.length)
             current_index = 0
         else
             current_index++
-        initiative_order[current_index].visual.setAttribute("current-turn", "")
+        initiatives[current_index].visual.set_current_turn(true)
     }
 
     const start = () => {
-        initiative_order[current_index].visual.setAttribute("current-turn", "")
-
-        initiative_order = initiative_order.sort((a, b) => a.initiative.value > b.initiative.value ? -1 : 1)
-        initiative_order.forEach(initiative => {
-            visual_initiative_order.add_creature({visual: initiative.visual})
-        })
+        initiatives[current_index].visual.set_current_turn(true)
     }
 
     return {
@@ -50,8 +59,8 @@ export const create_initiative_order = ({visual_initiative_order}: {
 }
 
 export type InitiativeOrder = {
-    start: () => void
     add_creature: (creature: Creature) => void
     get_current_creature: () => Creature
     next_turn: () => void
+    start: () => void
 }
--- src/scripts/initiative_order/InitiativeOrderVisual.ts
+++ src/scripts/initiative_order/InitiativeOrderVisual.ts
@@ -1,26 +1,48 @@
 import {Creature} from "scripts/battlegrid/creatures/Creature";
-
 import {ExprNumberResolved} from "scripts/expressions/evaluator/types";
 
-export class InitiativeOrderVisual {
-    create_creature = ({creature, initiative}: { creature: Creature, initiative: ExprNumberResolved }) => {
-        const html_initiative = document.createElement("div")
+export const create_visual_initiative_order = (): InitiativeOrderVisual => ({
+    create_creature: ({creature, initiative, index}: {
+        creature: Creature,
+        initiative: ExprNumberResolved,
+        index: number
+    }) => {
+        const html_initiative_order = document.getElementById("initiative_order")!
+        if ( html_initiative_order.children.length > index)
+            throw new AssertionError("create_visual_initiative_order must never be called with an index higher than its length")
+
+        const html_creature_initiative = document.createElement("div")
 
         const html_initiative_name = document.createElement("span")
         html_initiative_name.textContent = creature.data.name
-        html_initiative.appendChild(html_initiative_name)
+        html_creature_initiative.appendChild(html_initiative_name)
 
         const html_initiative_number = document.createElement("span")
         html_initiative_number.textContent = ` ${initiative.value}`
-        html_initiative.appendChild(html_initiative_number)
+        html_creature_initiative.appendChild(html_initiative_number)
 
-        html_initiative.setAttribute("creature-id", creature.data.name.toLowerCase())
+        html_creature_initiative.setAttribute("creature-id", creature.data.name.toLowerCase())
 
-        return html_initiative
+        if ( html_initiative_order.children.length === index)
+            html_initiative_order.appendChild(html_creature_initiative)
+        else if( html_initiative_order.children.length < index)
+            html_initiative_order.insertBefore(html_creature_initiative, html_creature_initiative.children[index])
+
+        return {
+            set_current_turn: (value: boolean) => {
+                if (value)
+                    html_creature_initiative.setAttribute("current-turn", "")
+                else
+                    html_creature_initiative.removeAttribute("current-turn")
+            }
+        }
     }
+})
 
-    add_creature = ({visual}: { visual: HTMLDivElement }) => {
-        const html_initiative_order = document.getElementById("initiative_order")!
-        html_initiative_order.appendChild(visual)
-    }
+export type InitiativeOrderVisual = {
+    create_creature: (props: { creature: Creature, initiative: ExprNumberResolved, index: number }) => CreatureInitiativeVisual
+}
+
+export type CreatureInitiativeVisual = {
+    set_current_turn: (value: boolean) => void
 }
