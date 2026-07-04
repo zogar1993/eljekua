55558cb99e832c10ecee74c932990134391c2f26
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-07-04 13:38:43 -0300

    :poop: add bunch of WIP code to evil ritualist

--- src/scripts/monsters/EvilRitualist.ts
+++ src/scripts/monsters/EvilRitualist.ts
@@ -1,4 +1,9 @@
-const evil_ritualist = {
+import {Size} from "scripts/creatures/SIZES";
+import {AttributeCode} from "scripts/character_sheet/attributes";
+import {DefenseCode} from "scripts/character_sheet/get_creature_defense";
+import {Power} from "scripts/types";
+
+const evil_ritualist: any = {
     template: "Evil Ritualist",
     size: "medium",
     race: "human",
@@ -60,7 +65,38 @@ const evil_ritualist = {
             type: "immediate_reaction",
             intercepts: "critical_hit",
             conditions: [
-                `$lower_or_equal($distance($triggerer(),owner),5)`,
+                `$is_lower_or_equal($distance($triggerer(),owner),5)`,
+                `$or($is_ally($triggerer()),$is_monster_template(owner.template))`,
+            ]
+            //TODO P1 melee basic attack against an adjacent enemy
+        }
+    ]
+}
+
+type Monster = {
+    template: string
+    size: Size
+    race: string
+    keywords: Array<string>
+    level: number
+    xp: number
+    archetypes: Array<string>
+    initiative: number
+    senses: Record<string, number>
+    alignment: string
+    languages: Array<string>
+    hp: number //TODO P1 a missed attack never damages a minion.
+    defenses: Record<DefenseCode, number>
+    speed: number
+    powers: Power
+    attributes: Record<AttributeCode, number>
+    triggers: [
+        {
+            name: "Unholy Vigor",
+            type: "immediate_reaction",
+            intercepts: "critical_hit",
+            conditions: [
+                `$is_lower_or_equal($distance($triggerer(),owner),5)`,
                 `$or($is_ally($triggerer()),$is_monster_template(owner.template))`,
             ]
             //TODO P1 melee basic attack against an adjacent enemy
