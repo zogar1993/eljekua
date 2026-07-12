0be4c0f4c7d18f7de226e72bf3571a78b6412460
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-07-12 12:27:33 -0300

    :wrench: fix jest configuration since tests weren't running

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,14 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "jest-environment-jsdom",
   transform: {
-    ...tsJestTransformCfg,
+    '^.+\\.tsx?$': 'ts-jest',
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/src/**/?(*.)+(spec|test).ts'],
   moduleNameMapper: {
     "^scripts/(.+)$": "<rootDir>/src/scripts/$1",
     "^tests/(.+)$": "<rootDir>/src/tests/$1",
   },
   setupFilesAfterEnv: ['jest-extended/all'],
+  transformIgnorePatterns: ['/node_modules/'],
 };
