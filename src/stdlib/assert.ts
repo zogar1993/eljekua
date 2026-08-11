d69e008d730a39cf8dcd722566a87e3eb02ca0a9
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-08-11 20:08:11 -0300

    :technologist: add assert_is_true to start simplifying assertions

--- src/stdlib/assert.ts
+++ src/stdlib/assert.ts
@@ -1,3 +1,9 @@
+import {AssertionError} from "stdlib/AssertionError";
+
 export function assert(value: boolean, message: () => string) {
     if(!value) throw Error(message())
+}
+
+export function assert_is_true(value: boolean) {
+    if (!value) throw new AssertionError("Expected true")
 }
