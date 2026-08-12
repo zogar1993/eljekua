5f761efecefd98770daf39fa3c321da30a524321
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-08-12 10:33:32 -0300

    :technologist: add assert_is_not_null

--- src/stdlib/assert.ts
+++ src/stdlib/assert.ts
@@ -7,3 +7,7 @@ export function assert(value: boolean, message: () => string) {
 export function assert_is_true(value: boolean) {
     if (!value) throw new AssertionError("Expected true")
 }
+
+export function assert_is_not_null<T>(value: T | null): asserts value is T {
+    if (value == null) throw new AssertionError("Expected not null")
+}
