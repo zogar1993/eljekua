8346c39a11f39833330cc43b79490436ee48fb2e
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-08-15 00:25:37 -0300

    :coffin: remove description from evaluate_string expression

--- src/core/expressions/evaluator/internals/evaluate_string.ts
+++ src/core/expressions/evaluator/internals/evaluate_string.ts
@@ -2,4 +2,4 @@ import type {ExprString} from "core/expressions/evaluator/types";
 import type {AstNodeString} from "core/expressions/parser/nodes/AstNodeString";
 
 export const evaluate_string = (node: AstNodeString): ExprString =>
-    ({type: "string", value: node.value, description: node.value})
+    ({type: "string", value: node.value})
