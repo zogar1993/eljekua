f5d509bd5fdd35ff98de20b4b3492c519eae173c
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-08-16 12:02:17 -0300

    :goal_net: add assertion for not undefined

--- src/stdlib/assert.ts
+++ src/stdlib/assert.ts
@@ -11,3 +11,7 @@ export function assert_is_true(value: boolean) {
 export function assert_is_not_null<T>(value: T | null): asserts value is T {
     if (value == null) throw new AssertionError("Expected not null")
 }
+
+export function assert_is_not_undefined<T>(value: T | undefined): asserts value is T {
+    if (value === undefined) throw new AssertionError("Expected not undefined")
+}
