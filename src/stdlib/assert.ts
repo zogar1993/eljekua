a68b032682c2a67d6118498010437471786306ec
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-08-28 21:19:30 -0300

    :goal_net: add assert_is_valid_index function to stdlib

--- src/stdlib/assert.ts
+++ src/stdlib/assert.ts
@@ -1,7 +1,7 @@
 import {AssertionError} from "stdlib/AssertionError";
 
 export function assert(value: boolean, message: () => string) {
-    if(!value) throw Error(message())
+    if (!value) throw Error(message())
 }
 
 export function assert_is_true(value: boolean) {
@@ -14,4 +14,8 @@ export function assert_is_not_null<T>(value: T | null): asserts value is T {
 
 export function assert_is_not_undefined<T>(value: T | undefined): asserts value is T {
     if (value === undefined) throw new AssertionError("Expected not undefined")
+}
+
+export function assert_is_valid_index(index: number, array: Array<unknown>) {
+    if (index < 0 || index >= array.length) throw Error(`Index ${index} is out of bounds for array of length ${array.length}`)
 }
