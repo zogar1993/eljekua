2e1db1747d1be3eea87f4215928cb79262949c50
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-09-02 20:35:18 -0300

    :goal_net: add assert_is_included to check if an array contains an item

--- src/stdlib/assert.ts
+++ src/stdlib/assert.ts
@@ -19,3 +19,9 @@ export function assert_is_not_undefined<T>(value: T | undefined): asserts value 
 export function assert_is_valid_index(index: number, array: Array<unknown>) {
     if (index < 0 || index >= array.length) throw Error(`Index ${index} is out of bounds for array of length ${array.length}`)
 }
+
+export function assert_is_included<T>(item: T, array: Array<T>) {
+    if (!array.includes(item)) throw Error(`Item not included in array.\n Item: ${to_json(item)}\n Array: ${to_json(array)}`)
+}
+
+const to_json = (value: unknown) => JSON.stringify(value, null, 2)
