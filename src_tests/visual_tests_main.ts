b3542a96a592b5da35f14c77e973943130080f3e
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-09-11 10:25:46 -0300

    :children_crossing: remove vm state and action log from tests

--- src_tests/visual_tests_main.ts
+++ src_tests/visual_tests_main.ts
@@ -1,11 +1,9 @@
-import {create_action_log} from "web/core/action_log/ActionLog";
 import {create_hit_status_buttons_ui} from "web/core/hit_status_buttons/HitStatusButtonsUI";
 import {create_initiative_order_ui} from "web/core/initiative_order/InitiativeOrderUI";
 import {create_add_creature_to_game} from "core/use_cases/add_creature_to_game";
 import {create_start_battle} from "core/use_cases/start_battle";
 import {create_instruction_loop} from "core/instruction_loop";
 import {build_evaluate_ast} from "core/virtual_machine/expressions/evaluate_ast";
-import {create_instruction_visualizer} from "web/core/instruction_visualizer/instruction_visualizer";
 import {create_set_current_turn_to_creature} from "core/use_cases/gameplay/set_current_turn_to_creature";
 import {create_game_events} from "core/events/GameEvents";
 import {create_game_state} from "core/game_state/GameState";
@@ -24,8 +22,6 @@ const battle_grid_ui = initialize_battle_grid_ui({game_state, game_events, game_
 create_option_buttons_ui({game_events, game_input: instruction_loop})
 create_hit_status_buttons_ui({game_events, game_inputs: instruction_loop})
 create_initiative_order_ui({game_events})
-create_instruction_visualizer({game_events})
-create_action_log({game_events})
 
 const set_current_turn_to_creature = create_set_current_turn_to_creature({game_state, game_events})
 const add_creature_to_game = create_add_creature_to_game({game_state, game_events})
