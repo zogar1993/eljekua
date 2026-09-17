588d93295ddd2a3348cd7de910f1f1bbeeb3d67c
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-09-17 19:07:26 -0300

    :children_crossing: now only primary targets need confirmation if only one character is selected. Other targets are selected automatically.

--- src/core/virtual_machine/instructions/interpreters/interpret_select_target.ts
+++ src/core/virtual_machine/instructions/interpreters/interpret_select_target.ts
@@ -31,38 +31,35 @@ export const interpret_select_target = ({
     const owner = EXPR.as_creature(vm_state.get_variable(SYSTEM_KEYWORD.OWNER))
     const target_label = instruction.target_label
 
-    if (clickable.length === 1) {
-        if (vm_state.peek().type !== "attack_dice_roll") {
-            const position = clickable[0]
+    if (clickable.length === 1 && instruction.target_label !== SYSTEM_KEYWORD.PRIMARY_TARGET) {
+        const position = clickable[0]
 
-            if (instruction.targeting_type === "area_burst") {
-                assert_is_footprint_one(position)
-                const distance = instruction.radius
-                const highlighted_area = get_reach_area_burst({origin: position, distance, battle_grid})
-                const target_positions = highlighted_area.filter(p => battle_grid.is_terrain_occupied(p))
-                const targets = battle_grid.get_creatures_in_positions(target_positions)
+        if (instruction.targeting_type === "area_burst") {
+            assert_is_footprint_one(position)
+            const distance = instruction.radius
+            const highlighted_area = get_reach_area_burst({origin: position, distance, battle_grid})
+            const target_positions = highlighted_area.filter(p => battle_grid.is_terrain_occupied(p))
+            const targets = battle_grid.get_creatures_in_positions(target_positions)
 
-                vm_state.set_variable(target_label, {type: "creatures", value: targets})
-            } else if (instruction.targeting_type === "movement") {
-                // TODO P2 automatic resolution for movement feels odd when its a movement action, but not when its a secondary action
-                const path = get_shortest_path({creature: owner, destination: position, battle_grid})
+            vm_state.set_variable(target_label, {type: "creatures", value: targets})
+        } else if (instruction.targeting_type === "movement") {
+            const path = get_shortest_path({creature: owner, destination: position, battle_grid})
 
-                vm_state.set_variable(target_label, {type: "positions", value: path})
-            } else if (instruction.targeting_type === "push") {
+            vm_state.set_variable(target_label, {type: "positions", value: path})
+        } else if (instruction.targeting_type === "push") {
+            vm_state.set_variable(target_label, {type: "positions", value: [position]})
+        } else {
+            if (instruction.target_type === "terrain") {
                 vm_state.set_variable(target_label, {type: "positions", value: [position]})
+            } else if ((instruction.target_type === "creature" || instruction.target_type === "enemy")) {
+                const creature = battle_grid.get_creature_by_position(position)
+                vm_state.set_variable(target_label, {type: "creatures", value: [creature]})
             } else {
-                if (instruction.target_type === "terrain") {
-                    vm_state.set_variable(target_label, {type: "positions", value: [position]})
-                } else if ((instruction.target_type === "creature" || instruction.target_type === "enemy")) {
-                    const creature = battle_grid.get_creature_by_position(position)
-                    vm_state.set_variable(target_label, {type: "creatures", value: [creature]})
-                } else {
-                    throw Error(`instruction not valid: targeting_type '${instruction.targeting_type}' target_type '${instruction.target_type}'`)
-                }
+                throw Error(`instruction not valid: targeting_type '${instruction.targeting_type}' target_type '${instruction.target_type}'`)
             }
-
-            return
         }
+
+        return
     }
 
     const get_attack_hit_chance_against = (creature: Creature) => {
