cb1bae877829e9847bca4fa90ce1f396b5db2654
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-09-22 00:42:27 -0300

    :recycle: give cleaner names to damage modifier tests

--- src/tests/damage/damage_modifiers.spec.ts
+++ src/tests/damage/damage_modifiers.spec.ts
@@ -1,12 +1,11 @@
 import {
     transform_power_ir_into_vm_representation
 } from "core/expressions/parser/transform_power_ir_into_vm_representation";
-import {FIRE_ATTACK, FIRE_NECROTIC_ATTACK, UNTYPED_DAMAGE} from "tests/damage/test_powers";
 import {create_creature_test_helpers} from "tests/utils/creature_test_helpers";
 import {create_test_game} from "tests/utils/create_test_game";
 
-const DEFAULT_POSITION = {x: 3, y: 4, footprint: 1} as const
-const DEFENDER_POSITION = {x: 4, y: 4, footprint: 1} as const
+const POSITION_LINUAR = {x: 3, y: 4, footprint: 1} as const
+const POSITION_RAGOZ = {x: 4, y: 4, footprint: 1} as const
 
 let start_battle: ReturnType<typeof create_test_game>["start_battle"]
 let given_a_creature_is_created: ReturnType<typeof create_creature_test_helpers>["given_a_creature_is_created"]
@@ -31,106 +30,84 @@ beforeEach(() => {
 
 describe("damage resistance", () => {
     test("typed damage is reduced against resistant creatures", () => {
-        given_a_creature_is_created({
-            name: "Attacker",
-            position: DEFAULT_POSITION,
-            powers: [transform_power_ir_into_vm_representation(FIRE_ATTACK)],
-        })
-        given_a_creature_is_created({
-            name: "Defender",
-            position: DEFENDER_POSITION,
-            resistances: {fire: 1},
-        })
+        given_a_creature_is_created({name: "linuar", position: POSITION_LINUAR, powers: [DEAL_DAMAGE_FIRE]})
+        given_a_creature_is_created({name: "ragoz", position: POSITION_RAGOZ, resistances: {fire: 1}})
         start_battle()
-        when_creature("Attacker").selects_action("Fire Attack")
-        when_creature("Attacker").selects_target("Defender")
-        then_creature("Defender").has_hp(7)
+
+        when_creature("linuar").selects_action("Deal Damage")
+        when_creature("linuar").selects_target("ragoz")
+
+        then_creature("ragoz").has_hp(7)
     })
 
     test("untyped damage is not reduced by typed resistances", () => {
-        given_a_creature_is_created({
-            name: "Attacker",
-            position: DEFAULT_POSITION,
-            powers: [transform_power_ir_into_vm_representation(UNTYPED_DAMAGE)],
-        })
-        given_a_creature_is_created({
-            name: "Defender",
-            position: DEFENDER_POSITION,
-            resistances: {fire: 1},
-        })
+        given_a_creature_is_created({name: "linuar", position: POSITION_LINUAR, powers: [DEAL_DAMAGE_UNTYPED]})
+        given_a_creature_is_created({name: "ragoz", position: POSITION_RAGOZ, resistances: {fire: 1}})
         start_battle()
-        when_creature("Attacker").selects_action("Untyped Damage")
-        when_creature("Attacker").selects_target("Defender")
-        then_creature("Defender").has_hp(6)
+
+        when_creature("linuar").selects_action("Deal Damage")
+        when_creature("linuar").selects_target("ragoz")
+
+        then_creature("ragoz").has_hp(6)
     })
 
     test("dual type damage only considers lower resistance", () => {
-        given_a_creature_is_created({
-            name: "Attacker",
-            position: DEFAULT_POSITION,
-            powers: [transform_power_ir_into_vm_representation(FIRE_NECROTIC_ATTACK)],
-        })
-        given_a_creature_is_created({
-            name: "Defender",
-            position: DEFENDER_POSITION,
-            resistances: {fire: 1, necrotic: 2},
-        })
+        given_a_creature_is_created({name: "linuar", position: POSITION_LINUAR, powers: [DEAL_DAMAGE_FIRE_NECROTIC]})
+        given_a_creature_is_created({name: "ragoz", position: POSITION_RAGOZ, resistances: {fire: 1, necrotic: 2}})
         start_battle()
-        when_creature("Attacker").selects_action("Fire Necrotic Attack")
-        when_creature("Attacker").selects_target("Defender")
-        then_creature("Defender").has_hp(7)
+
+        when_creature("linuar").selects_action("Deal Damage")
+        when_creature("linuar").selects_target("ragoz")
+
+        then_creature("ragoz").has_hp(7)
     })
 })
 
 describe("damage vulnerability", () => {
     test("typed damage is increased against vulnerable creatures", () => {
-        given_a_creature_is_created({
-            name: "Attacker",
-            position: DEFAULT_POSITION,
-            powers: [transform_power_ir_into_vm_representation(FIRE_ATTACK)],
-        })
-        given_a_creature_is_created({
-            name: "Defender",
-            position: DEFENDER_POSITION,
-            resistances: {fire: -1},
-        })
+        given_a_creature_is_created({name: "linuar", position: POSITION_LINUAR, powers: [DEAL_DAMAGE_FIRE]})
+        given_a_creature_is_created({name: "ragoz", position: POSITION_RAGOZ, resistances: {fire: -1}})
         start_battle()
-        when_creature("Attacker").selects_action("Fire Attack")
-        when_creature("Attacker").selects_target("Defender")
-        then_creature("Defender").has_hp(5)
+
+        when_creature("linuar").selects_action("Deal Damage")
+        when_creature("linuar").selects_target("ragoz")
+
+        then_creature("ragoz").has_hp(5)
     })
 
     test("untyped damage is not increased by typed vulnerability", () => {
-        given_a_creature_is_created({
-            name: "Attacker",
-            position: DEFAULT_POSITION,
-            powers: [transform_power_ir_into_vm_representation(UNTYPED_DAMAGE)],
-        })
-        given_a_creature_is_created({
-            name: "Defender",
-            position: DEFENDER_POSITION,
-            resistances: {fire: -1},
-        })
+        given_a_creature_is_created({name: "linuar", position: POSITION_LINUAR, powers: [DEAL_DAMAGE_UNTYPED]})
+        given_a_creature_is_created({name: "ragoz", position: POSITION_RAGOZ, resistances: {fire: -1}})
         start_battle()
-        when_creature("Attacker").selects_action("Untyped Damage")
-        when_creature("Attacker").selects_target("Defender")
-        then_creature("Defender").has_hp(6)
+
+        when_creature("linuar").selects_action("Deal Damage")
+        when_creature("linuar").selects_target("ragoz")
+
+        then_creature("ragoz").has_hp(6)
     })
 
     test("dual type damage only considers higher vulnerability", () => {
-        given_a_creature_is_created({
-            name: "Attacker",
-            position: DEFAULT_POSITION,
-            powers: [transform_power_ir_into_vm_representation(FIRE_NECROTIC_ATTACK)],
-        })
-        given_a_creature_is_created({
-            name: "Defender",
-            position: DEFENDER_POSITION,
-            resistances: {fire: -1, necrotic: -2},
-        })
+        given_a_creature_is_created({name: "linuar", position: POSITION_LINUAR, powers: [DEAL_DAMAGE_FIRE_NECROTIC]})
+        given_a_creature_is_created({name: "ragoz", position: POSITION_RAGOZ, resistances: {fire: -1, necrotic: -2}})
         start_battle()
-        when_creature("Attacker").selects_action("Fire Necrotic Attack")
-        when_creature("Attacker").selects_target("Defender")
-        then_creature("Defender").has_hp(4)
+
+        when_creature("linuar").selects_action("Deal Damage")
+        when_creature("linuar").selects_target("ragoz")
+
+        then_creature("ragoz").has_hp(4)
     })
 })
+
+const deal_damage_power = ({damage_types}: {damage_types: Array<string>}) => transform_power_ir_into_vm_representation({
+    name: "Deal Damage",
+    type: {action: "standard", cooldown: "at-will", attack: true},
+    targeting: {targeting_type: "melee_weapon", target_type: "enemy", amount: 1},
+    effect: [
+        {type: "set_hit_status", target: "primary_target", status: 1},
+        {type: "apply_damage", value: "4", target: "primary_target", damage_types: damage_types},
+    ],
+})
+
+const DEAL_DAMAGE_UNTYPED = deal_damage_power({damage_types: []})
+const DEAL_DAMAGE_FIRE = deal_damage_power({damage_types: ["fire"]})
+const DEAL_DAMAGE_FIRE_NECROTIC = deal_damage_power({damage_types: ["fire", "necrotic"]})
